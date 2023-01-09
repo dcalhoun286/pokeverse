c38a4919a28fb1c6ad4a3cc3fdd6e3d5fafd34cf
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2023-01-09 15:26:31 -0800

    wrote state for managing pokemon data from the api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
 
@@ -6,6 +6,8 @@ const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+
+  const [pokeData, setPokeData] = useState([]);
 
   return (
     <div data-testid="app">
