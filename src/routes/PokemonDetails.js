ea97517405db27a2875062bd2e73e660bf142989
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2023-01-16 21:43:18 -0800

    conditional rendering based on state data in PokemonDetails

--- src/routes/PokemonDetails.js
+++ src/routes/PokemonDetails.js
@@ -3,11 +3,31 @@ import { useParams } from 'react-router-dom';
 
 function PokemonDetails() {
 
+    const [pokemon, setPokemon] = useState(null);
+
     const params = useParams();
 
+    useEffect(() => {
+        async () => {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.name}`);
+            const jsonData = await response.json();
+            console.log(`useEffect PokemonDetails: `, jsonData);
+            setPokemon(jsonData);
+        };
+    },[pokemon]);
+    
     return (
-        <>
-        </>
+        pokemon
+        ?   (
+                <React.Fragment>
+
+                </React.Fragment>
+            )
+        :   (
+                <React.Fragment>
+                    loading ...
+                </React.Fragment>
+            )
     )
 }
 
