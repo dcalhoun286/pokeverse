75f599935db215486bf9a88e270461377fce0364
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2023-01-16 20:46:37 -0800

    moved most of App logic into Home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { InputGroup, FormControl, Container, Row, Col } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 
 import { Navigation } from './components/Navigation';
-import { PokemonCard } from './components/PokemonCard';
+import { Home } from './routes/Home';
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
@@ -10,7 +10,6 @@ const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 function App() {
 
   const [ pokeData, setPokeData ] = useState([]);
-  const [ filteredPokeData, setFilteredPokeData ] = useState([]);
 
   const fetchPokemonData = async () => {
     const response = await fetch(pokeApi);
@@ -18,24 +17,6 @@ function App() {
     const pokemonArray = responseData.results;
     console.log('pokeApi response data', pokemonArray);
     setPokeData(pokemonArray);
-    setFilteredPokeData(pokemonArray);
-  };
-
-  const handlePokemonSearch = (e) => {
-    e.preventDefault();
-
-    if (e.target.value.length) {
-      const regex = new RegExp(e.target.value, 'gi');
-      const filtered = pokeData.filter(pokemon => {
-        return pokemon.name.match(regex) || pokemon.name.toLowerCase().includes(e.target.value.toLowerCase());
-      });
-
-      setFilteredPokeData(filtered);
-
-    } else {
-      setFilteredPokeData([...pokeData]);
-    }
-
   };
 
   useEffect(() => {
@@ -49,49 +30,7 @@ function App() {
           <Navigation />
         </Row>
 
-        <Row>
-          <InputGroup>
-            <FormControl
-              placeholder='Enter a pokemon'
-              aria-label='Enter a pokemon'
-              onChange={handlePokemonSearch}
-            />
-          </InputGroup>
-        </Row>
-
-        <Row>
-          <h1
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              paddingTop: '5px',
-              paddingBottom: '5px',
-              marginTop: '8px'
-            }}
-          >
-            Pokemon should appear here
-          </h1>
-        </Row>
-
-        <Row>
-
-          {
-            filteredPokeData.length && filteredPokeData.map(pokemon => (
-
-              <Col className='col-12 col-sm-6 col-md-4 col-xl-3 my-1'>
-                <PokemonCard
-                  key={pokemon.name}
-                  name={pokemon.name}
-                  url={pokemon.url}
-                />
-              </Col>
-
-            ))
-          }
-
-        </Row>
-
-        <PokemonCard />
+        <Home pokeData={pokeData} />
       </div>
     </Container>
   );
