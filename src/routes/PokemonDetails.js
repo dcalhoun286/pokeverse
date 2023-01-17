b1fd50accf821d4cf8fadefb6f52802b5d89d161
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2023-01-16 20:57:42 -0800

    initial imports and structure of PokemonDetails

---
+++ src/routes/PokemonDetails.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+function PokemonDetails() {
+
+    const params = useParams();
+
+    return (
+        <>
+        </>
+    )
+}
+
+export { PokemonDetails };
