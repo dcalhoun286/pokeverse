a4292b48ab1863fb7f0d81019b2204ddf3e9b645
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2023-01-20 12:34:41 -0800

    created state and passing it in FavoritesContext.Provider

--- src/components/context/FavoritesProvider.js
+++ src/components/context/FavoritesProvider.js
@@ -1,10 +1,13 @@
-import React, { createContext } from 'react';
+import React, { useState, createContext } from 'react';
 
 export const FavoritesContext = createContext();
 
 function FavoritesProvider ({ children }) {
+
+    const [ favorites, setFavorites ] = useState([]);
+
     return (
-        <FavoritesContext.Provider>
+        <FavoritesContext.Provider value={{favorites}}>
             {children}
         </FavoritesContext.Provider>
     )
